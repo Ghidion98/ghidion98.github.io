64023236a0a6081ab2ad83efa5b6b9ac1771c939
Man_17 <naniluis282@gmail.com>
2025-04-14 11:35:53 -0400

    Updated files

--- scripts/app.js
+++ scripts/app.js
@@ -60,3 +60,21 @@ function showProject(name) {
  }
 
  startTimer();
+
+ // Scroll to top button
+ const toTop = document.getElementById('toTop');
+ window.addEventListener('scroll', () => {
+    if(window.scrollY > 300) {
+       toTop.style.display = 'block';
+    }else {
+        toTop.style.display = 'none';
+    }
+ });
+
+ toTop.addEventListener('click',(e) => {
+    e.preventDefault();
+    window.scrollTo({
+       top: 0,
+       behavior: 'smooth'
+    });
+ });
